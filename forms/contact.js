759fb22996226c96c972857ab0009b9fd0577448
Rahulraj <rahulrohiniraju@gmail.com>
2022-02-18 09:47:05 +0530

    title

--- forms/contact.js
+++ forms/contact.js
@@ -91,3 +91,55 @@ $(document).ready(function() {
       });
     });
   })
+
+
+  
+// Document is ready
+$(document).ready(function () {
+    
+  // Validate Username
+      $('#usercheck').hide();   
+      let usernameError = true;
+      $('#usernames').keyup(function () {
+          validateUsername();
+      });
+       
+      function validateUsername() {
+        let usernameValue = $('#usernames').val();
+        if (usernameValue.length == '') {
+        $('#usercheck').show();
+            usernameError = false;
+            return false;
+        }
+        else if((usernameValue.length < 3)||
+                (usernameValue.length > 10)) {
+            $('#usercheck').show();
+            $('#usercheck').html
+  ("**length of username must be between 3 and 10");
+            usernameError = false;
+            return false;
+        }
+        else {
+            $('#usercheck').hide();
+        }
+      }
+   
+     // Validate Email
+      const email =
+          document.getElementById('email');
+      email.addEventListener('blur', ()=>{
+         let regex =
+  /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+         let s = email.value;
+         if(regex.test(s)){
+            email.classList.remove(
+                  'is-invalid');
+            emailError = true;
+          }
+          else{
+              email.classList.add(
+                    'is-invalid');
+              emailError = false;
+          }
+      })
+    });
